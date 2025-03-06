c64eb6ef305fc1712bc2d723f339f4b4deb90dbd
Dylan Wu <dylanwu@10-23-3-33.dynapool.nyu.edu>
2025-03-06 05:21:12 -0500

    lvl2+3+fail+finish

--- script.js
+++ script.js
@@ -3,4 +3,24 @@ function gotolvl1() {
     window.location.href = "leveluno.html";
 }
 
+function gotolvl2() {
+
+    window.location.href = "leveldos.html";
+}
+
+function gotoFail() {
+
+    window.location.href = "failscreen.html";
+}
+
+function gotoEnd() {
+
+    window.location.href = "endscreen.html";
+}
+
+function gotoStart() {
+
+    window.location.href = "remix.html";
+}
+
 
